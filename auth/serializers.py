9fe56fa6cbc3d0f32347f9cae7039e882af74d42
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-15 13:34:37 +0530

    added serializers

---
+++ auth/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+from auth.models import *
+from django.contrib.auth.models import User
+
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=['first_name', 'last_name', 'username', 'email', 'password']
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=['first_name', 'last_name', 'username']
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model=Profile
+        fields=['user', 'name', 'bio', 'created_on']
+        read_only_fields=['user']
+
+
