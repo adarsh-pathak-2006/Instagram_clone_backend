80d5560cded85bb0d8dbc99eb7c900290fea71d9
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-15 15:19:30 +0530

    added posts as well as reels...urls for now..no actual storage

--- auth/models.py
+++ auth/models.py
@@ -6,8 +6,11 @@ class Profile(models.Model):
     name=models.CharField(max_length=120)
     bio=models.TextField(null=True)
     created_on=models.DateTimeField(auto_now_add=True)
+    post_liked=models.ManyToManyField("posts.Post", related_name='profile_post_liked', null=True)
+    reel_liked=models.ManyToManyField("posts.Reel", related_name='profile_reel_liked', null=True)
     
     def __str__(self):
         return self.user.username
 
 
+#serializers abhi shi krna hai dhyam rkhna
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from auth.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    title=models.CharField(max_length=300)
+    description=models.TextField(null=True)
+    attachment=models.URLField(null=True)
+    created=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title[:100]
+    
+class Reel(models.Model):
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reels')
+    video=models.URLField()
+    title=models.CharField(max_length=300)
+    description=models.TextField(null=True)
+
+    def __str__(self):
+        return self.title[:100]
